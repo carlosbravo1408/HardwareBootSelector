0beb8aff5501cfb494d52f6b337b5811611d6426
CarlosBravo <carlos.j.bravo@hotmail.com>
2022-10-30 23:09:31 -0500

    refactor: Add Nokia5110 implementation and HID class

--- main.c
+++ main.c
@@ -31,56 +31,117 @@
 #include "tusb.h"
 #include "pico/stdlib.h"
 
+#include "usb_descriptors.h"
+
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/spi.h"
+#include "Nokia5110/Nokia5110.h"
+#include "Nokia5110/stencil.h"
+
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
 //--------------------------------------------------------------------+
 
+#define SPI_PORT spi0
+
+#define LCD_CS   17
+#define LCD_SCK  18
+#define LCD_MOSI 19
+#define LCD_RST  21
+#define LCD_DC   20
+
 #define SWITCH_PIN 28
+#define BACKLIGHT 16
+#define BUTTON1 1
 
 /* Blink pattern
  * - 250 ms  : device not mounted
  * - 1000 ms : device mounted
  * - 2500 ms : device is suspended
  */
-enum  {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
+enum {
+    BLINK_NOT_MOUNTED = 250,
+    BLINK_MOUNTED = 1000,
+    BLINK_SUSPENDED = 2500,
 };
 
 
 static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+static float conversion_factor = (3.3f / (1 << 12));
+static uint16_t LCD_REFRESH = 500;
 
 void led_blinking_task(void);
+
 void cdc_task(void);
 
-uint8_t read_switch_value()
-{  
-  return gpio_get(SWITCH_PIN) ? '1' : '0';
+float calculate_temperature(void);
+
+void draw_display_task(void);
+
+int current_os = '0';
+
+uint8_t read_switch_value() {
+    return gpio_get(SWITCH_PIN) ? '1' : '0';
+}
+
+uint8_t button_read() {
+    return gpio_get(BUTTON1) ? 1 : 0;
 }
 
 /*------------- MAIN -------------*/
-int main(void)
-{
+int main(void) {
 
-  gpio_init(SWITCH_PIN);
-	gpio_set_dir(SWITCH_PIN, false);
-  gpio_set_pulls (SWITCH_PIN,false,true);
-  
-  
-  board_init();
-  tusb_init();
-  
+    gpio_init(SWITCH_PIN);
+    gpio_set_dir(SWITCH_PIN, false);
+    gpio_set_pulls(SWITCH_PIN, false, true);
 
-  while (1)
-  {
-    tud_task(); // tinyusb device task
-    led_blinking_task();
+    gpio_init(BUTTON1);
+    gpio_set_dir(BUTTON1, false);
+    gpio_set_pulls(BUTTON1, false, true);
 
-    cdc_task();
-  }
+    adc_init();
+    adc_gpio_init(26);
+    adc_set_temp_sensor_enabled(true);
+    adc_select_input(4);
 
-  return 0;
+    spi_init(SPI_PORT, 4000 * 1000);
+
+    gpio_set_function(LCD_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(LCD_MOSI, GPIO_FUNC_SPI);
+
+    gpio_init(LCD_RST);
+    gpio_set_dir(LCD_RST, GPIO_OUT);
+    gpio_init(LCD_CS);
+    gpio_set_dir(LCD_CS, GPIO_OUT);
+    gpio_init(LCD_DC);
+    gpio_set_dir(LCD_DC, GPIO_OUT);
+    gpio_init(BACKLIGHT);
+    gpio_set_dir(BACKLIGHT, GPIO_OUT);
+    gpio_put(BACKLIGHT, 0);
+    Nokia5110_Init();
+
+    board_init();
+    tusb_init();
+    tud_init(BOARD_DEVICE_RHPORT_NUM);
+    current_os = read_switch_value();
+    tud_remote_wakeup();
+
+    while (1) {
+        tud_task(); // tinyusb device task
+
+        led_blinking_task();
+
+        cdc_task();
+
+        hid_task();
+
+        led_blinking_task();
+
+        draw_display_task();
+    }
+
+    return 0;
 }
 
 //--------------------------------------------------------------------+
@@ -88,94 +149,260 @@ int main(void)
 //--------------------------------------------------------------------+
 
 // Invoked when device is mounted
-void tud_mount_cb(void)
-{
-  blink_interval_ms = BLINK_MOUNTED;
+void tud_mount_cb(void) {
+    blink_interval_ms = BLINK_MOUNTED;
 }
 
 // Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-  blink_interval_ms = BLINK_NOT_MOUNTED;
+void tud_umount_cb(void) {
+    blink_interval_ms = BLINK_NOT_MOUNTED;
 }
 
 // Invoked when usb bus is suspended
 // remote_wakeup_en : if host allow us  to perform remote wakeup
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-  (void) remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
+void tud_suspend_cb(bool remote_wakeup_en) {
+    (void) remote_wakeup_en;
+    blink_interval_ms = BLINK_SUSPENDED;
 }
 
 // Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-  blink_interval_ms = BLINK_MOUNTED;
+void tud_resume_cb(void) {
+    blink_interval_ms = BLINK_MOUNTED;
 }
 
 
 //--------------------------------------------------------------------+
 // USB CDC
 //--------------------------------------------------------------------+
-void cdc_task(void)
-{
-  // connected() check for DTR bit
-  // Most but not all terminal client set this when making connection
-  // if ( tud_cdc_connected() )
-  {
-    // connected and there are data available
-    if ( tud_cdc_available() )
+void cdc_task(void) {
+    // connected() check for DTR bit
+    // Most but not all terminal client set this when making connection
+    // if ( tud_cdc_connected() )
     {
-      // read datas
-      char buf[64];
-      uint32_t count = tud_cdc_read(buf, sizeof(buf));
-      (void) count;
+        // connected and there are data available
+        if (tud_cdc_available()) {
+            // read datas
+            char buf[64];
+            uint32_t count = tud_cdc_read(buf, sizeof(buf));
+            (void) count;
 
-      // Echo back
-      // Note: Skip echo by commenting out write() and write_flush()
-      // for throughput test e.g
-      //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
-      tud_cdc_write(buf, count);
-      tud_cdc_write_flush();
+            // Echo back
+            // Note: Skip echo by commenting out write() and write_flush()
+            // for throughput test e.g
+            //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
+            tud_cdc_write(buf, count);
+            tud_cdc_write_flush();
+        }
     }
-  }
 }
 
 // Invoked when cdc when line state changed e.g connected/disconnected
-void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
-{
-  (void) itf;
-  (void) rts;
+void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
+    (void) itf;
+    (void) rts;
 
-  // TODO set some indicator
-  if ( dtr )
-  {
-    // Terminal connected
-  }else
-  {
-    // Terminal disconnected
-  }
+    // TODO set some indicator
+    if (dtr) {
+        // Terminal connected
+    } else {
+        // Terminal disconnected
+    }
 }
 
 // Invoked when CDC interface received data from host
-void tud_cdc_rx_cb(uint8_t itf)
-{
-  (void) itf;
+void tud_cdc_rx_cb(uint8_t itf) {
+    (void) itf;
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+static void send_hid_report(uint8_t report_id, uint32_t btn) {
+    // skip if hid is not ready yet
+    if (!tud_hid_ready()) return;
+
+    switch (report_id) {
+        case REPORT_ID_KEYBOARD: {
+            // use to avoid send multiple consecutive zero report for keyboard
+            static bool has_keyboard_key = false;
+
+            if (btn) {
+                uint8_t keycode[6] = {0};
+                keycode[0] = HID_KEY_A;
+
+                tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+                has_keyboard_key = true;
+            } else {
+                // send empty key report if previously has key pressed
+                if (has_keyboard_key) tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+                has_keyboard_key = false;
+            }
+        }
+            break;
+
+        case REPORT_ID_MOUSE: {
+            int8_t const delta = 5;
+
+            // no button, right + down, no scroll, no pan
+            tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta, delta, 0, 0);
+        }
+            break;
+
+        case REPORT_ID_CONSUMER_CONTROL: {
+            // use to avoid send multiple consecutive zero report
+            static bool has_consumer_key = false;
+
+            if (btn) {
+                // volume down
+                uint16_t volume_down = HID_USAGE_CONSUMER_VOLUME_DECREMENT;
+                tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &volume_down, 2);
+                has_consumer_key = true;
+            } else {
+                // send empty key report (release key) if previously has key pressed
+                uint16_t empty_key = 0;
+                if (has_consumer_key) tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &empty_key, 2);
+                has_consumer_key = false;
+            }
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+// Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
+// tud_hid_report_complete_cb() is used to send the next report after previous one is complete
+void hid_task(void) {
+    // Poll every 10ms
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
+
+    if (board_millis() - start_ms < interval_ms) return; // not enough time
+    start_ms += interval_ms;
+
+    uint32_t const btn = button_read();
+
+    // Remote wakeup
+    if (tud_suspended() && btn) {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
+    } else {
+        // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
+        send_hid_report(REPORT_ID_CONSUMER_CONTROL, btn);
+    }
+}
+
+// Invoked when sent REPORT successfully to host
+// Application can use this to send the next report
+// Note: For composite reports, report[0] is report ID
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, /*uint16_t*/ uint8_t len) {
+    (void) instance;
+    (void) len;
+
+    uint8_t next_report_id = report[0] + 1u;
+
+    if (next_report_id < REPORT_ID_COUNT) {
+        send_hid_report(next_report_id, button_read());
+    }
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer,
+                               uint16_t reqlen) {
+    // TODO not Implemented
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer,
+                           uint16_t bufsize) {
+    (void) instance;
+
+    if (report_type == HID_REPORT_TYPE_OUTPUT) {
+        // Set keyboard LED e.g Capslock, Numlock etc...
+        if (report_id == REPORT_ID_KEYBOARD) {
+            // bufsize should be (at least) 1
+            if (bufsize < 1) return;
+
+            uint8_t const kbd_leds = buffer[0];
+
+            if (kbd_leds & KEYBOARD_LED_CAPSLOCK) {
+                // Capslock On: disable blink, turn led on
+                blink_interval_ms = 0;
+                board_led_write(true);
+            } else {
+                // Caplocks Off: back to normal blink
+                board_led_write(false);
+                blink_interval_ms = BLINK_MOUNTED;
+            }
+        }
+    }
 }
 
 //--------------------------------------------------------------------+
 // BLINKING TASK
 //--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
+void led_blinking_task(void) {
+    static uint32_t start_ms = 0;
+    static bool led_state = false;
 
-  // Blink every interval ms
-  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
-  start_ms += blink_interval_ms;
+    // Blink every interval ms
+    if (board_millis() - start_ms < blink_interval_ms) return; // not enough time
+    start_ms += blink_interval_ms;
 
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
+    board_led_write(led_state);
+    led_state = 1 - led_state; // toggle
+}
+
+void draw_display_task(void) {
+
+    static uint32_t start_ms = 0;
+
+    // Blink every interval ms
+    if (board_millis() - start_ms < LCD_REFRESH) return; // not enough time
+    start_ms += LCD_REFRESH;
+    clearDisplay();
+    drawBitmap(0, 0, stencil_1, 83, 47, BLACK);
+    setCursor(1, 2);
+    setTextSize(1);
+    printString("Temp:");
+    setCursor(33, 2);
+    char numStr[16];
+    float temperature = calculate_temperature();
+    sprintf(numStr, "%.*f", 2, temperature);
+    printString(numStr);
+    setCursor(1, 14);
+    printString("Cfg Os:");
+    setCursor(45, 14);
+    printString(current_os == '0' ? "UBUNTU" : "Win-10");
+    setCursor(1, 26);
+    printString("NextOs:");
+    setCursor(45, 26);
+    printString(read_switch_value() == '0' ? "UBUNTU" : "Win-10");
+    setCursor(1, 38);
+    printString("Status:");
+    drawBitmap(42, 36,
+               blink_interval_ms == BLINK_NOT_MOUNTED ? UNMOUNTED : blink_interval_ms == BLINK_SUSPENDED ? SUSPENDED
+                                                                                                         : MOUNTED, 41,
+               11, BLACK);
+    display();
+}
+
+
+float calculate_temperature(void) {
+    uint16_t adcValue = adc_read();
+    float reading = adcValue * conversion_factor;
+    return (27 - (reading - 0.706) / 0.001721);
 }
